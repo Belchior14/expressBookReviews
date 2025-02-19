ff1d6efb0373f7a946f830c77cce5ce8d982d129
Belchior Fontão <93472115+Belchior14@users.noreply.github.com>
2025-02-19 11:56:38 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,21 +31,51 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
 
+  let filterAuths = {}
+
+  let author = req.params.author;
+
+   for (const key in books) {
+        if (books[key].author === author) {
+            filterAuths[key] = books[key];
+        }
+    }
+
 
   
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({filterAuths});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  let filterTitles = {}
+
+  let title = req.params.title;
+
+   for (const key in books) {
+        if (books[key].title === title) {
+            filterTitles[key] = books[key];
+        }
+    }
+
+
+  
+
+  return res.status(300).json({filterTitles});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+
+    let isbnNumber = req.params.isbn
+
+    let book = books[isbnNumber].reviews;
+
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({book});
 });
 
 module.exports.general = public_users;
