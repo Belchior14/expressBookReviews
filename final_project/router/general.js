dbcc4ed62e5b5d24cd650571845419c79cabe98d
Belchior Fontão <93472115+Belchior14@users.noreply.github.com>
2025-02-19 10:27:00 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,18 +13,26 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ let isbnNumber = req.params.isbn
+
+ let book = books[isbnNumber];
+
+ return res.status(300).json(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+
+
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
